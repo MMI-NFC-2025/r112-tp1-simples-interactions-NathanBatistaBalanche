bec5119c669099472b44e89f3f6fe980c6fe35db
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2025-11-05 15:54:17 +0100

    Exercice : Réagir à un click

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,9 @@
 // Exercice 1 : Mettre en rouge
 const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
 aMettreEnRouge.style.color = "red";
+
+// Exercice 2 : Reagir à un click
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
+enRougeSuiteAClick.addEventListener("click", (evt)=> {
+  enRougeSuiteAClick.style.color = "red";
+});
