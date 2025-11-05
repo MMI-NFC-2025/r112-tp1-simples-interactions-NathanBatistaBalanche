adf54cc7511200d44bb272d3f5e5eb5cbe8b3cda
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2025-11-05 15:56:23 +0100

    Exercice : Collection d'éléments

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,11 @@ const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
 enRougeSuiteAClick.addEventListener("click", (evt)=> {
   enRougeSuiteAClick.style.color = "red";
 });
+
+// Exercice 3 : 
+const collH2 = document.querySelectorAll("h2");
+collH2.forEach((elm)=> {
+    elm.addEventListener("click", (evt)=> {
+        evt.target.style.color = "red";
+    });
+});
